6542aaed7afbeb05aac4e6633d5a9a7f01aaf287
Kevanthony <Michael_Curry99@outlook.com>
2022-07-14 22:37:48 -0500

    done

--- script.js
+++ script.js
@@ -1,73 +1,35 @@
-// let button = document.getElementById('hey');
+let all = document.getElementById("all");         // This is the DOM to create the link to the pages I created in the buttons.
+let breakfast = document.getElementById("breakfast");       
+let lunch = document.getElementById("lunch");
+let shakes = document.getElementById("shakes");
+let dinner = document.getElementById("dinner");
 
-// button.addEventListener('click', function(){
-//     a.href = "new.html"; 
+all.addEventListener('click', function (){             // functions to make the button display the next page when clicked.
+    window.location.assign("index.html");          // inserts the link
+      window.location.replace("index.html");      // prevents browser history when going back and forth.
+    
+});
 
-// })
-// Test to see if it works
+breakfast.addEventListener('click', function (){ 
+   window.location.assign("breakfast.html"); 
+     window.location.replace("breakfast.html");
+   
+});
 
+lunch.addEventListener('click', function (){ 
+   window.location.assign("lunch.html"); 
+     window.location.replace("lunch.html");
+   
+});
 
-// function pressOne() {
-//     var a = document.createElement('a');
-//     var linkText = document.getElementById('all')
-//     a.appendChild(linkText);
-//   //   a.title = "my title text";
-//     a.href = "MenuSelector.html";
-//     document.body.appendChild(a);
-//   }
-//   pressOne()
+shakes.addEventListener('click', function (){ 
+   window.location.assign("shakes.html"); 
+     window.location.replace("shakes.html");
+   
+});
 
-//   function pressTwo() {
-//       var a = document.createElement('a');
-//     var linkText = document.getElementById('breakfast')
-//     a.appendChild(linkText);
-//   //   a.title = "my title text";
-//     a.href = "breakfast.html";
-//     document.body.appendChild(a);
-
-//   }
-//   pressTwo()
-
-
-// Test to see if it works (above).
-
-document.getElementById("all").addEventListener("click", urlOne);          // This is the DOM to create the link to the pages I created in the buttons.
-document.getElementById("breakfast").addEventListener("click", urlTwo);    // I use the id from the HTML button elements so that once I click a button, I can go to the next page.
-document.getElementById("lunch").addEventListener("click", urlThree);      // The urlOne to urlFive are functions used to hold the action the buttons are needed to do, along with the click event.
-document.getElementById("shakes").addEventListener("click", urlFour);
-document.getElementById("dinner").addEventListener("click", urlFive);
-
-// this is the "All" button that
-function urlOne() {                                        // function line for each button. 
-       window.location.assign("index.html"); // inserts the link
-       window.location.replace("index.html"); // prevents from having a history on each button I click.
-       return false; // The prevention to erase history when clicking the back and forward button.
-    }
-
-    function urlTwo() {
-      window.location.assign("breakfast.html");
-      window.location.replace("breakfast.html");
-       return false;
-   }
-
-   function urlThree() {
-    window.location.assign("lunch.html");
-    window.location.replace("lunch.html");
-       return false;
-   }
-
-   function urlFour() {
-    window.location.assign("shakes.html");
-    window.location.replace("shakes.html");
-    return false;
-   }
-
-   function urlFive() {
-    window.location.assign("dinner.html");
-    window.location.replace("dinner.html");
-    return false;
-   }
-
-   let container = document.getElementById("main-container");
-function handleForm(event) { event.preventDefault(); } 
-container.addEventListener('submit', handleForm);
+dinner.addEventListener('click', function (){ 
+   window.location.assign("dinner.html"); 
+     window.location.replace("dinner.html");
+   
+});
